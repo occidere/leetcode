aedf4fc8f98dc462b7e194e7ffa82373468f7ed3
occidere <occidere@naver.com>
2019-08-28 22:07:53 +0900

    108번 문제 solve

---
+++ Algorithms/108_Convert_Sorted_Array_to_Binary_Search_Tree/solution.py
@@ -0,0 +1,22 @@
+from typing import *
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        def build(left: int, right: int) -> Optional[TreeNode]:
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            root = TreeNode(nums[mid])
+            root.left = build(left, mid - 1)
+            root.right = build(mid + 1, right)
+            return root
+        return build(0, len(nums) - 1)
