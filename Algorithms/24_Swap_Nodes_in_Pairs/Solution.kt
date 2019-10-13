3756815564a8c208844990029477ece88be68ae1
S. J. Lee <occidere@naver.com>
2019-10-13 21:26:45 +0900

    인덴트 수정

--- Algorithms/24_Swap_Nodes_in_Pairs/Solution.kt
+++ Algorithms/24_Swap_Nodes_in_Pairs/Solution.kt
@@ -11,24 +11,24 @@ import java.util.*
  */
 class Solution {
     fun swapPairs(head: ListNode?): ListNode? {
-	var h = head
+        var h = head
         val stack: Stack<ListNode> = Stack()
         while (h != null) {
-	    stack.push(h)
-	    h = h.next
+            stack.push(h)
+            h = h.next
         }
 
-	if (stack.size % 2 == 1) {
-	    h = stack.pop()
-	}
+        if (stack.size % 2 == 1) {
+            h = stack.pop()
+        }
 
-	while (stack.isNotEmpty()) {
-	    val n1 = stack.pop()
-	    val n2 = stack.pop()
-	    n2.next = h
-	    n1.next = n2
-	    h = n1
-	}
+        while (stack.isNotEmpty()) {
+            val n1 = stack.pop()
+            val n2 = stack.pop()
+            n2.next = h
+            n1.next = n2
+            h = n1
+        }
 
 	return h
     }
