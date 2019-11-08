74378b1f7b6b6753d10a54d762bb0eef46e396e9
occidere <occidere@naver.com>
2019-11-08 20:26:13 +0900

    226번 문제 solve (Python3)

---
+++ Algorithms/226_Invert_Binary_Tree/solution.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root is None:
+            return root
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        root.left, root.right = root.right, root.left
+        return root
