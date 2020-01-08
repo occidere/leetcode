793642d3100264301a387ced07ccc150243fe872
occidere <occidere@naver.com>
2020-01-08 22:41:47 +0900

    1047번 문제 solve (C11)

---
+++ Algorithms/1047_Remove_All_Adjacent_Duplicates_In_String/solution.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* removeDuplicates(char* S){
+    int k = 0, n = strlen(S);
+    char* stk = malloc(20001);
+
+    for (int i = 0; i < n; i++) {
+        if (k > 0 && S[i] == stk[k - 1]) {
+            k--;
+        } else {
+            stk[k++] = S[i];
+        }
+    }
+    stk[k] = NULL;
+
+    return stk;
+}
