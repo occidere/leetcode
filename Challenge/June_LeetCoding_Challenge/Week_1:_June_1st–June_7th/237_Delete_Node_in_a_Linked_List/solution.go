cf8b1955a499d6e2018ab650e29b7ba288b1dab2
occidere <occidere@naver.com>
2020-06-02 18:56:38 +0900

    237번 문제 solve (Go)

---
+++ Challenge/June_LeetCoding_Challenge/Week_1:_June_1st–June_7th/237_Delete_Node_in_a_Linked_List/solution.go
@@ -0,0 +1,12 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+    *node = *node.Next
+}
