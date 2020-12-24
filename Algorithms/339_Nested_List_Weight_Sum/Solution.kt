14bebdb79a0fca9390842d81fce9fc2a40830abd
occidere <occidere@naver.com>
2020-12-24 19:15:57 +0900

    339번 문제 solve (Kotlin)

---
+++ Algorithms/339_Nested_List_Weight_Sum/Solution.kt
@@ -0,0 +1,41 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     constructor()
+ *
+ *     // Constructor initializes a single integer.
+ *     constructor(value: Int)
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     fun isInteger(): Boolean
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     fun getInteger(): Int?
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     fun setInteger(value: Int): Unit
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     fun add(ni: NestedInteger): Unit
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     fun getList(): List<NestedInteger>?
+ * }
+ */
+class Solution {
+    private var acc = 0
+    fun depthSum(nestedList: List<NestedInteger>, depth: Int = 1): Int {
+        for (ni in nestedList) {
+            if (ni.isInteger()) {
+                acc += depth * ni.getInteger()!!
+            } else {
+                depthSum(ni.getList()!!, depth + 1)
+            }
+        }
+        return acc
+    }
+}
