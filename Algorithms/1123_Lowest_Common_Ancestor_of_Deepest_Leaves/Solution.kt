ff5494cc56d01af6c0ebe9bdca13f236ee89d62e
occidere <occidere@naver.com>
2021-08-28 19:47:42 +0900

    1123번 문제 solve (Kotlin)

---
+++ Algorithms/1123_Lowest_Common_Ancestor_of_Deepest_Leaves/Solution.kt
@@ -0,0 +1,38 @@
+import kotlin.math.max
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun lcaDeepestLeaves(root: TreeNode): TreeNode {
+        val (parents, nodes) = mutableMapOf<Int, TreeNode>() to mutableMapOf<Int, MutableList<TreeNode>>()
+
+        fun TreeNode.dfs(level: Int = 0): Int {
+            var deepestLevel = level
+            nodes.putIfAbsent(level, mutableListOf())
+            nodes[level]!! += this
+            left?.also {
+                parents[it.`val`] = this
+                deepestLevel = max(deepestLevel, it.dfs(level + 1))
+            }
+            right?.also {
+                parents[it.`val`] = this
+                deepestLevel = max(deepestLevel, it.dfs(level + 1))
+            }
+            return deepestLevel
+        }
+
+        var leaves = nodes[root.dfs()]!!.toSet()
+        while (leaves.size != 1) {
+            leaves = leaves.map { parents[it.`val`]!! }.toSet()
+        }
+        return leaves.first()
+    }
+}
