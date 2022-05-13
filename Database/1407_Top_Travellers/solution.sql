5430d49046d23c4480836a64d5ff9d182b68fecc
Sungjun Lee <occidere@naver.com>
2022-05-13 23:20:35 +0900

    1407번 문제 solve (Oracle)

---
+++ Database/1407_Top_Travellers/solution.sql
@@ -0,0 +1,16 @@
+/* Write your PL/SQL query statement below */
+SELECT
+    a.name,
+    SUM(
+        CASE
+            WHEN b.distance IS NULL THEN 0
+            ELSE b.distance
+        END
+    ) travelled_distance
+FROM users a
+    LEFT JOIN rides b ON a.id = b.user_id
+GROUP BY a.name
+ORDER BY
+    travelled_distance DESC,
+    a.name ASC
+;
