bfb067b4c63516783985598bf7ced86aa0834821
Sungjun Lee <occidere@naver.com>
2022-05-13 23:37:01 +0900

    1527번 문제 solve (Oracle)

---
+++ Database/1527_Patients_With_a_Condition/solution.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+SELECT
+    patient_id,
+    patient_name,
+    conditions
+FROM patients
+WHERE
+    conditions LIKE 'DIAB1%'
+    OR conditions LIKE '% DIAB1%'
+;
