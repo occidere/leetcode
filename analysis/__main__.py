5d5ce7873691411e723517813c0768d385b3c194
occidere <occidere@naver.com>
2022-06-01 23:11:52 +0900

    Added job parameter

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -1,18 +1,25 @@
 import argparse
 
+from logger.logger import error
 from job.language_statistics import LanguageStatisticsJob
 from job.leetcode_ranking import LeetCodeRankingJob
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
+	parser.add_argument('--job', help='Job name')
 	parser.add_argument('--github-api-token', help='Input GitHub API Token')
 	parser.add_argument('--line-bot-id', help='Line bot ID to send push message')
 	parser.add_argument('--line-channel-access-token', help='Line channel access token to send push message')
 	args = parser.parse_args()
 
-	LanguageStatisticsJob(args.github_api_token).run()
-	LeetCodeRankingJob(
-		args.github_api_token,
-		args.line_bot_id,
-		args.line_channel_access_token
-	).run()
+	job_name = args.job
+	if job_name == 'LanguageStatisticsJob':
+		LanguageStatisticsJob(args.github_api_token).run()
+	elif job_name == 'LeetCodeRankingJob':
+		LeetCodeRankingJob(
+			args.github_api_token,
+			args.line_bot_id,
+			args.line_channel_access_token
+		).run()
+	else:
+		error(f'No such job: {job_name}')
