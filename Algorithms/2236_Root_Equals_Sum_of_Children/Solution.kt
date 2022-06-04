bd9b613d5b2cb4f8707f0be989ec5523a432ba07
Sungjun Lee <occidere@naver.com>
2022-06-04 18:36:43 +0900

    2236번 문제 solve (Kotlin)

---
+++ Algorithms/2236_Root_Equals_Sum_of_Children/Solution.kt
@@ -0,0 +1,13 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun checkTree(root: TreeNode) = root.`val` == root.left!!.`val` + root.right!!.`val`
+}
