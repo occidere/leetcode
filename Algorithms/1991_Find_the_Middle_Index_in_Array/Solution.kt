a0295e61d97343e9f651c858f3178debb3f44564
Sungjun Lee <occidere@naver.com>
2022-08-26 19:04:01 +0900

    1991번 문제 solve (Kotlin)

---
+++ Algorithms/1991_Find_the_Middle_Index_in_Array/Solution.kt
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int lefts = 0, rights = Arrays.stream(nums).sum() - nums[0], n = nums.length - 1;
+        for (int i = 0; i < n; ++i) {
+            if (lefts == rights) {
+                return i;
+            }
+            lefts += nums[i];
+            rights -= nums[i + 1];
+        }
+        return lefts == rights ? n : -1;
+    }
+}
