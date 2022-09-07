63bd06906cfa8255067196f0df69e10b7eae7457
Sungjun Lee <occidere@naver.com>
2022-09-07 19:06:20 +0900

    2396번 문제 solve (Kotlin)

---
+++ Algorithms/2396_Strictly_Palindromic_Number/Solution.kt
@@ -0,0 +1,9 @@
+/**
+ * @author occidere
+ * @Blog: https://blog.naver.com/occidere
+ * @Github: https://github.com/occidere
+ * @since 2022-09-07
+ */
+class Solution {
+    fun isStrictlyPalindromic(n: Int) = false
+}
