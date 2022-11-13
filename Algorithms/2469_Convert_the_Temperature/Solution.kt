38504226e6d965ea7470d1776e66792354606b64
Sungjun Lee <occidere@naver.com>
2022-11-13 14:22:38 +0900

    2469번 문제 solve (Kotlin)

---
+++ Algorithms/2469_Convert_the_Temperature/Solution.kt
@@ -0,0 +1,9 @@
+/**
+ * @author occidere
+ * @Blog: https://blog.naver.com/occidere
+ * @Github: https://github.com/occidere
+ * @since 2022-11-13
+ */
+class Solution {
+    fun convertTemperature(celsius: Double) = doubleArrayOf(celsius + 273.15, celsius * 1.8 + 32)
+}
