1be149a6365390ef3de1d4bb9808fc996fd1b7a3
Sungjun Lee <occidere@naver.com>
2025-12-11 09:15:11 +0900

    Solved 3467

---
+++ Algorithms/3467_Transform_Array_by_Parity/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int[] transformArray(int[] nums) {
+        return Arrays.stream(nums)
+            .map(x -> x % 2)
+            .sorted()
+            .toArray();
+    }
+}
