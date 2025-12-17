4a60bf7008d803d4f37ef71153dfecd037d082c8
Sungjun Lee <occidere@naver.com>
2025-12-17 09:49:58 +0900

    Solved 3498

---
+++ Algorithms/3498_Reverse_Degree_of_a_String/Solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int reverseDegree(String s) {
+        return IntStream.range(0, s.length())
+            .map(i -> (i + 1) * (26 - (s.charAt(i) - 'a')))
+            .sum();
+    }
+}
